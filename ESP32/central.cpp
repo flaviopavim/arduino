0dd9f6f2d8963d2898a4463c4555452eb4011373
Flávio Pavim <contato@flaviopavim.com.br>
2024-02-20 01:53:58 -0300

    Updates

--- ESP32/central.cpp
+++ ESP32/central.cpp
@@ -1,6 +1,6 @@
 /*
 
-    Central ESP32
+    Central ESP32 + Alexa
 
     Alarm, garden control
 
@@ -24,11 +24,11 @@
 #include <WiFiClient.h>
 
 //Alexa
-#ifdef ARDUINO_ARCH_ESP32
-#include <WiFi.h>
-#else
-#include <ESP8266WiFi.h>
-#endif
+//#ifdef ARDUINO_ARCH_ESP32
+//#include <WiFi.h>
+//#else
+//#include <ESP8266WiFi.h>
+//#endif
 #include <Espalexa.h>
 
 #include <ArduinoJson.h>
@@ -37,6 +37,181 @@ String api = "http://whitehats.com.br/api/security/";
 const char *ssid=""; // Nome do WiFi
 const char *pass=""; // Senha do WiFi
 const char *uuid="abc123";   // ID registrado na API
+
+
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+#define PAUSE 0
+
+// change this to make the song slower or faster
+int tempo=144; 
+
+// change this to whichever pin you want to use
+//int buzzer = 11;
+
+// notes of the moledy followed by the duration.
+// a 4 means a quarter note, 8 an eighteenth , 16 sixteenth, so on
+// !!negative numbers are used to represent dotted notes,
+// so -4 means a dotted quarter note, that is, a quarter plus an eighteenth!!
+int melody[] = {
+
+  //Based on the arrangement at https://www.flutetunes.com/tunes.php?id=192
+  
+  NOTE_E5, 4,  NOTE_B4,8,  NOTE_C5,8,  NOTE_D5,4,  NOTE_C5,8,  NOTE_B4,8,
+  NOTE_A4, 4,  NOTE_A4,8,  NOTE_C5,8,  NOTE_E5,4,  NOTE_D5,8,  NOTE_C5,8,
+  NOTE_B4, -4,  NOTE_C5,8,  NOTE_D5,4,  NOTE_E5,4,
+  NOTE_C5, 4,  NOTE_A4,4,  NOTE_A4,8,  NOTE_A4,4,  NOTE_B4,8,  NOTE_C5,8,
+
+  NOTE_D5, -4,  NOTE_F5,8,  NOTE_A5,4,  NOTE_G5,8,  NOTE_F5,8,
+  NOTE_E5, -4,  NOTE_C5,8,  NOTE_E5,4,  NOTE_D5,8,  NOTE_C5,8,
+  NOTE_B4, 4,  NOTE_B4,8,  NOTE_C5,8,  NOTE_D5,4,  NOTE_E5,4,
+  NOTE_C5, 4,  NOTE_A4,4,  NOTE_A4,4, PAUSE, 4,
+
+  NOTE_E5, 4,  NOTE_B4,8,  NOTE_C5,8,  NOTE_D5,4,  NOTE_C5,8,  NOTE_B4,8,
+  NOTE_A4, 4,  NOTE_A4,8,  NOTE_C5,8,  NOTE_E5,4,  NOTE_D5,8,  NOTE_C5,8,
+  NOTE_B4, -4,  NOTE_C5,8,  NOTE_D5,4,  NOTE_E5,4,
+  NOTE_C5, 4,  NOTE_A4,4,  NOTE_A4,8,  NOTE_A4,4,  NOTE_B4,8,  NOTE_C5,8,
+
+  NOTE_D5, -4,  NOTE_F5,8,  NOTE_A5,4,  NOTE_G5,8,  NOTE_F5,8,
+  NOTE_E5, -4,  NOTE_C5,8,  NOTE_E5,4,  NOTE_D5,8,  NOTE_C5,8,
+  NOTE_B4, 4,  NOTE_B4,8,  NOTE_C5,8,  NOTE_D5,4,  NOTE_E5,4,
+  NOTE_C5, 4,  NOTE_A4,4,  NOTE_A4,4, PAUSE, 4,
+  
+  NOTE_E5,2,  NOTE_C5,2,
+  NOTE_D5,2,   NOTE_B4,2,
+  NOTE_C5,2,   NOTE_A4,2,
+  NOTE_GS4,2,  NOTE_B4,4,  PAUSE,8, 
+  NOTE_E5,2,   NOTE_C5,2,
+  NOTE_D5,2,   NOTE_B4,2,
+  NOTE_C5,4,   NOTE_E5,4,  NOTE_A5,2,
+  NOTE_GS5,2,
+
+};
+
+// sizeof gives the number of bytes, each int value is composed of two bytes (16 bits)
+// there are two values per note (pitch and duration), so for each note there are four bytes
+int notes=sizeof(melody)/2/2; 
+
+// this calculates the duration of a whole note in ms (60s/tempo)*4 beats
+int wholenote = (60000 * 4) / tempo;
+
+int divider = 0, noteDuration = 0;
+
+void tetris() {
+  // iterate over the notes of the melody. 
+  // Remember, the array is twice the number of notes (notes + durations)
+  for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
+
+    // calculates the duration of each note
+    divider = melody[thisNote + 1];
+    if (divider > 0) {
+      // regular note, just proceed
+      noteDuration = (wholenote) / divider;
+    } else if (divider < 0) {
+      // dotted notes are represented with negative durations!!
+      noteDuration = (wholenote) / abs(divider);
+      noteDuration *= 1.5; // increases the duration in half for dotted notes
+    }
+
+    // we only play the note for 90% of the duration, leaving 10% as a pause
+    tone(D5, melody[thisNote], noteDuration*0.9);
+
+    // Wait for the specief duration before playing the next note.
+    delay(noteDuration);
+    
+    // stop the waveform generation before the next note.
+    noTone(D5);
+  }
+}
+
+
 
 bool bool_alarm=false;
 
@@ -47,9 +222,6 @@ boolean connectWifi();
 void alarmChanged(uint8_t brightness);
 void computerChanged(uint8_t brightness);
 void waterChanged(uint8_t brightness);
-//void thirdLightChanged(uint8_t brightness);
-//void fourthLightChanged(uint8_t brightness);
-//void fifthLightChanged(uint8_t brightness);
 
 ESP8266WiFiMulti WiFiMulti;
 
@@ -60,20 +232,6 @@ String parameter="";
 boolean wifiConnected = false;
 
 Espalexa espalexa;
-
-void lightChanged(int port, uint8_t brightness) {
-  if (brightness) {
-    if (brightness == 255) {
-      digitalWrite(port, LOW);
-      Serial.println(port);
-      Serial.println("Device OFF");
-    }
-  } else {
-    digitalWrite(port, HIGH);
-    Serial.println(port);
-    Serial.println("Device ON");
-  }
-}
 
 void alarmChanged(uint8_t brightness) {
   if (brightness) {
@@ -119,18 +277,6 @@ void waterChanged(uint8_t brightness) {
     digitalWrite(port, LOW);
     Serial.println("Water OFF");
   }
-}
-
-void thirdLightChanged(uint8_t brightness) {
-  lightChanged(D3,brightness);
-}
-
-void fourthLightChanged(uint8_t brightness) {
-  lightChanged(D4,brightness);
-}
-
-void fifthLightChanged(uint8_t brightness) {
-  lightChanged(D5,brightness);
 }
 
 // connect to wifi – returns true if successful or false if not
@@ -211,16 +357,22 @@ void soundNotOk() {
 
 void soundTask() {
   rgb(255,255,255);
-  bip(1200,100);
+  bip(NOTE_C5,200);
   rgb(0,0,0);
-  bip(1800,100);
-  rgb(0,0,255);
-  bip(1400,100);
-  rgb(0,0,0);
-  bip(1000,100);
+  bip(NOTE_D5,200);
   rgb(255,255,255);
-  bip(1600,100);
+  bip(NOTE_E5,200);
   rgb(0,0,0);
+  bip(NOTE_F5,400);
+  rgb(255,255,255);
+  delay(25);
+  rgb(0,0,0);
+  bip(NOTE_F5,200);
+  rgb(255,255,255);
+  delay(25);
+  rgb(0,0,0);
+  bip(NOTE_F5,200);
+  rgb(255,255,255);
 }
 
 
@@ -253,6 +405,7 @@ void setup() {
     noTone(D5);
 
     soundSetup();
+    tetris();
 
     //RGB
     pinMode(D6, OUTPUT);
@@ -386,6 +539,7 @@ void loop() {
               //const char* to = doc["to"];
               //const char* system = doc["system"];
               //const char* version = doc["version"];
+              datetime = String(doc["datetime"]);
               const char* status = doc["status"];
               //const char* msg = doc["msg"];
 
@@ -431,12 +585,25 @@ void loop() {
                 bool_water=true;
               }
 
+              */
+
+              Serial.println(datetime);
+
               String year  = datetime.substring(0,4);
-              String month = datetime.substring(6,7);
-              String day   = datetime.substring(9,10);
-              String hour  = datetime.substring(12,13);
-              String min   = datetime.substring(15,16);
-              String sec   = datetime.substring(18,19);
+              String month = datetime.substring(5,7);
+              String day   = datetime.substring(8,10);
+              String hour  = datetime.substring(11,13);
+              String min   = datetime.substring(14,16);
+              String sec   = datetime.substring(17,19);
+
+              /*
+              Serial.println(year);
+              Serial.println(month);
+              Serial.println(day);
+              Serial.println(hour);
+              Serial.println(min);
+              Serial.println(sec);
+              */
 
               //liga a água às 05:00, 11:00 e 17:00
               if ((hour=="05" || hour=="11" || hour=="17") && min=="00") {
@@ -466,11 +633,11 @@ void loop() {
                     bool_water=false;
                     //desliga a água
                     digitalWrite(D2, HIGH);
-                    Serial.println("Device2 OFF");
+                    Serial.println("Water OFF");
                 }
 
               }
-              */
+              
 
             }
           } else {
