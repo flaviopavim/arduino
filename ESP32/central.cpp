beac9bc7a64e2ae21ea80de7de4e8aad1fe36d21
Flávio Pavim <contato@flaviopavim.com.br>
2024-02-19 23:10:11 -0300

    Updates

--- ESP32/central.cpp
+++ ESP32/central.cpp
@@ -4,6 +4,17 @@
 
     Alarm, garden control
 
+    API UNDER CONSTRUCTION - COMING SOON
+
+    D1 = Alarm
+    D2 = Computer
+    D3 = Water
+    D4 = 
+    D5 = 
+    D6 = R
+    D7 = G
+    D8 = B
+
 */
 
 #include <Arduino.h>
@@ -22,7 +33,7 @@
 
 #include <ArduinoJson.h>
 
-String api = "";
+String api = "http://whitehats.com.br/api/security/";
 const char *ssid=""; // Nome do WiFi
 const char *pass=""; // Senha do WiFi
 const char *uuid="abc123";   // ID registrado na API
@@ -31,31 +42,18 @@ const char *uuid="abc123";   // ID registrado na API
 boolean connectWifi();
 
 //callback functions
-void zeroLightChanged(uint8_t brightness);
-void firstLightChanged(uint8_t brightness);
-void secondLightChanged(uint8_t brightness);
-void thirdLightChanged(uint8_t brightness);
-void fourthLightChanged(uint8_t brightness);
-void fifthLightChanged(uint8_t brightness);
-//void sixthLightChanged(uint8_t brightness);
-//void seventhLightChanged(uint8_t brightness);
-//void eigthLightChanged(uint8_t brightness);
+void alarmChanged(uint8_t brightness);
+void computerChanged(uint8_t brightness);
+void waterChanged(uint8_t brightness);
+//void thirdLightChanged(uint8_t brightness);
+//void fourthLightChanged(uint8_t brightness);
+//void fifthLightChanged(uint8_t brightness);
 
 ESP8266WiFiMulti WiFiMulti;
 
 String datetime="";
 String command="";
-
-// device names
-String Device_0_Name = "esp 0";
-String Device_1_Name = "esp 1";
-String Device_2_Name = "esp 2";
-String Device_3_Name = "esp 3";
-String Device_4_Name = "esp 4";
-String Device_5_Name = "esp 5";
-//String Device_6_Name = "esp 6";
-//String Device_7_Name = "esp 7";
-//String Device_8_Name = "esp 8";
+String parameter="";
 
 boolean wifiConnected = false;
 
@@ -75,12 +73,38 @@ void lightChanged(int port, uint8_t brightness) {
   }
 }
 
-void zeroLightChanged(uint8_t brightness) {
-  lightChanged(D0,brightness);
+void alarmChanged(uint8_t brightness) {
+  if (brightness) {
+    if (brightness == 255) {
+      soundOk();
+      Serial.println("Alarm on");
+    }
+  } else {
+    soundNotOk();
+    Serial.println("Alarm off");
+  }
 }
 
-void firstLightChanged(uint8_t brightness) {
-  lightChanged(D1,brightness);
+void computerChanged(uint8_t brightness) {
+  int port=D1;
+  if (brightness) {
+    if (brightness == 255) {
+      command="abort-shutdown";
+      parameter="0";
+      digitalWrite(port, HIGH);
+      Serial.println("Computer ON");
+    }
+  } else {
+    command="shutdown";
+    parameter="3600";
+    digitalWrite(port, LOW);
+    Serial.println(port);
+    Serial.println("Computer will whutdown in 1 hour");
+  }
+}
+
+void waterChanged(uint8_t brightness) {
+  
 }
 
 void secondLightChanged(uint8_t brightness) {
@@ -143,20 +167,89 @@ boolean connectWifi() {
     Serial.println(ssid);
     Serial.print("IP address: ");
     Serial.println(WiFi.localIP());
-  }
-  else {
+  } else {
     Serial.println("Connection failed.");
   }
   return state;
 }
 
-/*
-void rgb(int r, int g, int b) {
-    analogWrite(R6, r);
-    analogWrite(R7, g);
-    analogWrite(R8, b);
+long map(long x, long in_min, long in_max, long out_min, long out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
-*/
+
+int ledMax=30;
+
+void rgb(int r, int g, int b) {
+    analogWrite(D6, map(r,0,255,0,ledMax));
+    analogWrite(D7, map(g,0,255,0,ledMax));
+    analogWrite(D8, map(b,0,255,0,ledMax));
+}
+
+void bip(int frequency, int delay_){
+  tone(D5, frequency);
+  delay(delay_);
+  noTone(D5);
+}
+
+void soundSetup() {
+  bip(500,100);
+  bip(1000,100);
+  bip(1500,100);
+}
+
+void soundOk() {
+  rgb(0,255,0);
+  bip(1000,100);
+  rgb(0,0,0);
+  bip(1200,100);
+  rgb(0,255,0);
+  bip(1400,100);
+  rgb(0,0,0);
+  bip(1600,100);
+  rgb(0,255,0);
+  bip(1800,100);
+  rgb(0,0,0);
+}
+
+void soundNotOk() {
+  rgb(255,0,0);
+  bip(1800,100);
+  rgb(0,0,0);
+  bip(1600,100);
+  rgb(255,0,0);
+  bip(1400,100);
+  rgb(0,0,0);
+  bip(1200,100);
+  rgb(255,0,0);
+  bip(1000,100);
+  rgb(0,0,0);
+}
+
+void soundTask() {
+  rgb(255,255,255);
+  bip(1200,100);
+  rgb(0,0,0);
+  bip(1800,100);
+  rgb(0,0,255);
+  bip(1400,100);
+  rgb(0,0,0);
+  bip(1000,100);
+  rgb(255,255,255);
+  bip(1600,100);
+  rgb(0,0,0);
+}
+
+
+void soundAmbulance() {
+  bip(1000,400);
+  bip(1800,400);
+}
+
+void soundAlarm() {
+  bip(1000,100);
+  bip(1800,100);
+}
+
 
 void setup() {
 
@@ -170,51 +263,51 @@ void setup() {
     pinMode(D2, OUTPUT);
     pinMode(D3, OUTPUT);
     pinMode(D4, OUTPUT);
+
+    //buzzer
     pinMode(D5, OUTPUT);
+    noTone(D5);
+
+    soundSetup();
 
     //RGB
     pinMode(D6, OUTPUT);
     pinMode(D7, OUTPUT);
     pinMode(D8, OUTPUT);
 
-    //rgb(255,255,0); //yellow
+    rgb(255,255,0); //yellow
   
-    digitalWrite(D0, HIGH);
-    digitalWrite(D1, HIGH);
-    digitalWrite(D2, HIGH);
+    digitalWrite(D0, LOW); //  Computer
+    digitalWrite(D1, LOW); //  Alarm
+    digitalWrite(D2, HIGH); // Water (Relay = HIGH)
     digitalWrite(D3, HIGH);
     digitalWrite(D4, HIGH);
-    digitalWrite(D5, HIGH);
-    //digitalWrite(R6, HIGH);
-    //digitalWrite(R7, HIGH);
-    //digitalWrite(R8, HIGH);
-  
+    //digitalWrite(D5, HIGH);
   
       // Initialise wifi connection
     wifiConnected = connectWifi();
   
     if (wifiConnected) {
         
-        // Define your devices here.
-        espalexa.addDevice(Device_0_Name, zeroLightChanged); //simplest definition, default state off
-        espalexa.addDevice(Device_1_Name, firstLightChanged); //simplest definition, default state off
-        espalexa.addDevice(Device_2_Name, secondLightChanged);
-        espalexa.addDevice(Device_3_Name, thirdLightChanged);
-        espalexa.addDevice(Device_4_Name, fourthLightChanged);
-        espalexa.addDevice(Device_5_Name, fifthLightChanged);
-        //espalexa.addDevice(Device_6_Name, sixthLightChanged);
-        //espalexa.addDevice(Device_7_Name, seventhLightChanged);
-        //espalexa.addDevice(Device_8_Name, eigthLightChanged);
+        // Define devices
+        espalexa.addDevice("alarm", alarmChanged);
+        espalexa.addDevice("computer", computerChanged);
+        espalexa.addDevice("water", waterChanged);
         
         espalexa.begin();
+
+        soundOk();
   
     } else {
-        while (1) {
+        //while (1) {
             Serial.println("Cannot connect to WiFi. Please check data and reset the ESP.");
             delay(2500);
-        }
+            soundNotOk();
+        //}
     }
 }
+
+
 
 bool bool_water=false;
 int count_water=0;
@@ -223,7 +316,15 @@ int count=0;
 void loop() {
     espalexa.loop();
 
+    //soundAlarm();
+
     //jsonn();
+
+    if (wifiConnected) {
+      rgb(0,255,0);
+    } else {
+      rgb(255,0,0);
+    }
   
 
     if (count==100) { //em média a cada 10 segundos
@@ -234,6 +335,9 @@ void loop() {
         HTTPClient http;
         Serial.print("[HTTP] begin...\n");
         if (http.begin(client, api)) {  // HTTP
+
+          rgb(0,0,255);
+
           Serial.print("[HTTP] POST...\n");
 
           // Set content type to JSON
@@ -252,6 +356,14 @@ void loop() {
           // Create a nested JSON object for the "data" field
           JsonObject nestedData = jsonDocument.createNestedObject("data");
           nestedData["view"] = "refresh";
+
+          if (command!="") {
+            nestedData["uuid"] = "1wvsD2RkFwaMZE8RVvtWSSpc0qhVSKkY";
+            nestedData["com"] = command;
+            nestedData["parameter"] = parameter;
+            command="";
+            parameter="";
+          }
 
           // Serialize the JSON object to a string
           String jsonString;
@@ -286,7 +398,32 @@ void loop() {
               const char* status = doc["status"];
               //const char* msg = doc["msg"];
 
-              if (status=="success") {
+              if (String(status)=="success") {
+                
+                const char* com = doc["data"]["com"];
+                const char* par = doc["data"]["parameter"];
+
+                String str_com=String(com);
+
+                if (str_com!="") {
+                  soundTask();
+
+                  if (str_com=="alarm") {
+
+                  }
+                }
+
+                /*
+                parameter = String(par);
+                if (String(com)=="shutdown") {
+                  parameter = "3600";
+                } else if (String(com)=="abort-shutdown") {
+                  parameter = "0";
+                }
+
+                command = String(com);
+                */
+                
 
               }
 
@@ -347,9 +484,11 @@ void loop() {
             }
           } else {
             Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+            rgb(255,0,0);
           }
           http.end();
         } else {
+          rgb(255,0,0);
           Serial.printf("[HTTP] Unable to connect\n");
         }
       }
