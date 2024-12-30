52d7a805a0267a1e3ac8bd1f5b2ba2e2b2b26685
Flávio Pavim <flavio.pavim@primecontrol.com.br>
2024-12-30 14:18:26 -0300

    Updates

---
+++ TLC5940/tlc5940.cpp
@@ -0,0 +1,18 @@
+#include "Tlc5940.h" 
+void setup() {
+    Tlc.init(0);
+}
+
+void loop() {
+    for (int i = 0; i < 16; i++) {
+        for (int a = 0; i <= 4095; a++) {
+            Tlc.set(i, a);
+            Tlc.update();
+            delay(10);
+        }
+        delay(50);
+        Tlc.clear();
+        Tlc.update();
+        delay(50);
+    }
+}
