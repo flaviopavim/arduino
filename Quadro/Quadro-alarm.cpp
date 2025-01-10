e68f579012e47bd698c95c06a962d8e9e355f80a
Flávio Pavim <contato@flaviopavim.com.br>
2025-01-09 22:02:21 -0300

    Fixes

--- Quadro/Quadro-alarm.cpp
+++ Quadro/Quadro-alarm.cpp
@@ -673,18 +673,11 @@ void loop() {
 
         }
 
-        //if (seconds==0) {
-        //  resetFalled();
-        //}
-
         count++;
-        //Serial.println(count);
         if (count > 60*15) { // a cada quinze minutos busca a hora denovo
           count=0;
           resetFalled();
-          //if (!bool_get_hour) {
-            setTime();
-          //}
+        setTime();
         }
 
         actualColor=randColor(); //muda a cor a cada 1 segundo
