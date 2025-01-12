32900a6f9250859c08fcf868995098ca7511c1eb
Flávio Pavim <contato@flaviopavim.com.br>
2025-01-11 22:31:13 -0300

    Fixes

--- FourLegs/FourLegs.cpp
+++ FourLegs/FourLegs.cpp
@@ -1,41 +1,55 @@
-#include <Servo.h> 
+#include <Servo.h> // Include the Servo library to control servo motors
 
-int servopin1 = A0;
-int servopin2 = A1;
-int servopin3 = A2;
-int servopin4 = A3;
-int servopin5 = 13;
- 
+// Pin definitions for the servos
+int servopin1 = A0; // Pin for the front left servo
+int servopin2 = A1; // Pin for the front right servo
+int servopin3 = A2; // Pin for the rear right servo
+int servopin4 = A3; // Pin for the rear left servo
+int servopin5 = 13; // Pin for the axis servo
+
+// Servo objects for controlling each servo
 Servo servo1;  
 Servo servo2;
 Servo servo3;
 Servo servo4;
 Servo servo5;
- 
-int angle = 90;   // servo position in degrees 
 
-int correction_front_left=-5;
-int correction_front_right=-15;
-int correction_rear_right=-10;
-int correction_rear_left=5;
-int correction_axis=-8;
+// Default servo position in degrees
+int angle = 90;  
 
+// Correction values for individual servo alignment
+int correction_front_left = -5;
+int correction_front_right = -15;
+int correction_rear_right = -10;
+int correction_rear_left = 5;
+int correction_axis = -8;
+
+// Function to control the front-left servo
 void frontLeft(int angle_) {
-  servo1.write(angle-angle_+correction_front_left); //front left       
-}
-void frontRight(int angle_) {
-  servo2.write(angle+angle_+correction_front_right); //front right
-}
-void rearRight(int angle_) {
-  servo3.write(angle+angle_+correction_rear_right); //rear right       
-}
-void rearLeft(int angle_) {
-  servo4.write(angle-angle_+correction_rear_left); //rear left
-}
-void axis(int angle_) {
-  servo5.write(angle+angle_+correction_axis);
+  servo1.write(angle - angle_ + correction_front_left);
 }
 
+// Function to control the front-right servo
+void frontRight(int angle_) {
+  servo2.write(angle + angle_ + correction_front_right);
+}
+
+// Function to control the rear-right servo
+void rearRight(int angle_) {
+  servo3.write(angle + angle_ + correction_rear_right);
+}
+
+// Function to control the rear-left servo
+void rearLeft(int angle_) {
+  servo4.write(angle - angle_ + correction_rear_left);
+}
+
+// Function to control the axis servo
+void axis(int angle_) {
+  servo5.write(angle + angle_ + correction_axis);
+}
+
+// Set all servos to the standing position
 void stand() {
   axis(0);
   frontLeft(0);
@@ -44,6 +58,7 @@ void stand() {
   rearRight(0);
 }
 
+// Set all servos to the stay position
 void stay() {
   axis(0);
   frontLeft(30);
@@ -51,172 +66,85 @@ void stay() {
   rearLeft(-30);
   rearRight(-30);
 }
- 
+
 void setup() { 
+  // Attach each servo to its corresponding pin
   servo1.attach(servopin1); 
   servo2.attach(servopin2); 
   servo3.attach(servopin3); 
   servo4.attach(servopin4); 
   servo5.attach(servopin5); 
-  stay();
+  stay(); // Set servos to the initial stay position
 } 
- 
+
+// Function to perform a dancing movement
 void dancing() {
-  for(angle = 90-15; angle < 90+30; angle++) {
-    servo1.write(angle+correction_front_left); //front left          
-    servo2.write(angle+correction_front_right); //front right
-    servo3.write(angle+correction_rear_right); //rear right        
-    servo4.write(angle+correction_rear_left); //rear left
+  for (angle = 90 - 15; angle < 90 + 30; angle++) {
+    servo1.write(angle + correction_front_left);
+    servo2.write(angle + correction_front_right);
+    servo3.write(angle + correction_rear_right);
+    servo4.write(angle + correction_rear_left);
     delay(10);
   }
-  for(angle = 90+15; angle > 90-30; angle--) {
-    servo1.write(angle+correction_front_left); //front left          
-    servo2.write(angle+correction_front_right); //front right
-    servo3.write(angle+correction_rear_right); //rear right        
-    servo4.write(angle+correction_rear_left); //rear left
+  for (angle = 90 + 15; angle > 90 - 30; angle--) {
+    servo1.write(angle + correction_front_left);
+    servo2.write(angle + correction_front_right);
+    servo3.write(angle + correction_rear_right);
+    servo4.write(angle + correction_rear_left);
     delay(10);
   }
 }
 
+// Function to perform a zigzag movement
 void maizein() {
-  for(int i = -20; i < 20; i++) {
+  for (int i = -20; i < 20; i++) {
     frontLeft(i);
     rearRight(i);
-
     frontRight(i);
     rearLeft(i);
     delay(20);
   }
-
-  for(int i = 20; i > -20; i--) {
+  for (int i = 20; i > -20; i--) {
     frontLeft(i);
     rearRight(i);
-
     frontRight(i);
     rearLeft(i);
     delay(20);
   }
 }
 
-
-void walk2() {
-  for(int i = -20; i < 20; i++) {
-    frontLeft(i);
-    rearRight(i);
-
-    frontRight(i);
-    rearLeft(i);
-    delay(20);
-  }
-
-  for(int i = 20; i > -20; i--) {
-    frontLeft(i);
-    rearRight(i);
-
-    frontRight(i);
-    rearLeft(i);
-    delay(20);
-  }
-
-
-  
-}
-
-
-void doubleLegs() {
-    
-}
-
-bool bool_front_left=false;
-bool bool_front_right=true;
-bool bool_rear_left=false;
-bool bool_rear_right=true;
-bool bool_axis=false;
-
-int maximum=15;
-
-double count_front_left=0;
-double count_front_right=15;
-double count_rear_left=0;
-double count_rear_right=15;
-double count_axis=0;
-
+// Walk movement logic using sine-like oscillation
 void walk() {
+  // Update the direction of movement for each servo based on limits
+  if (count_front_left > maximum) bool_front_left = true;
+  if (count_front_left < -maximum) bool_front_left = false;
+  if (count_front_right > maximum) bool_front_right = true;
+  if (count_front_right < -maximum) bool_front_right = false;
+  if (count_rear_left > maximum) bool_rear_left = true;
+  if (count_rear_left < -maximum) bool_rear_left = false;
+  if (count_rear_right > maximum) bool_rear_right = true;
+  if (count_rear_right < -maximum) bool_rear_right = false;
+  if (count_axis > maximum) bool_axis = true;
+  if (count_axis < -maximum) bool_axis = false;
 
-  if (count_front_left>maximum) {
-    bool_front_left=true;
-  }
-  if (count_front_left<-maximum) {
-    bool_front_left=false;
-  }
+  // Adjust servo positions based on direction
+  count_front_left += bool_front_left ? -1 : 1;
+  count_front_right += bool_front_right ? -1 : 1;
+  count_rear_left += bool_rear_left ? -1 : 1;
+  count_rear_right += bool_rear_right ? -1 : 1;
+  count_axis += bool_axis ? -1 : 1;
 
-  if (count_front_right>maximum) {
-    bool_front_right=true;
-  }
-  if (count_front_right<-maximum) {
-    bool_front_right=false;
-  }
-
-  if (count_rear_left>maximum) {
-    bool_rear_left=true;
-  }
-  if (count_rear_left<-maximum) {
-    bool_rear_left=false;
-  }
-
-  if (count_rear_right>maximum) {
-    bool_rear_right=true;
-  }
-  if (count_rear_right<-maximum) {
-    bool_rear_right=false;
-  }
-
-  if (count_axis>maximum) {
-    bool_axis=true;
-  }
-  if (count_axis<-maximum) {
-    bool_axis=false;
-  }
-  
-
-  if (bool_front_left) {
-    count_front_left--;
-  } else {
-    count_front_left++;
-  }
-  if (bool_front_right) {
-    count_front_right--;
-  } else {
-    count_front_right++;
-  }
-  if (bool_rear_left) {
-    count_rear_left--;
-  } else {
-    count_rear_left++;
-  }
-  if (bool_rear_right) {
-    count_rear_right--;
-  } else {
-    count_rear_right++;
-  }
-
-  if (bool_axis) {
-    count_axis--;
-  } else {
-    count_axis++;
-  }
-
-  frontLeft(count_front_left+15);
-  frontRight(count_front_right+15);
-  rearLeft(count_rear_left-15);
-  rearRight(count_rear_right-15);
-
+  // Move servos to calculated positions
+  frontLeft(count_front_left + 15);
+  frontRight(count_front_right + 15);
+  rearLeft(count_rear_left - 15);
+  rearRight(count_rear_right - 15);
   axis(count_axis);
 
-  delay(20);
-
+  delay(20); // Delay to control the speed of movement
 }
 
+// Sit-stand-stay sequence
 void sitStandStay() {
   sit();
   delay(2000);
@@ -228,6 +156,7 @@ void sitStandStay() {
   delay(2000);
 }
 
+// Set all servos to the sitting position
 void sit() {
   axis(0);
   frontLeft(60);
@@ -237,7 +166,7 @@ void sit() {
 }
 
 void loop() {
-  walk();
+  walk(); // Execute walking behavior
 
   //stand();
 
